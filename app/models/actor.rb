718b151625b941c660824db536ad28cd2a7f615a
github username <github email address>
2017-07-12 16:54:41 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,23 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+
+  def full_name
+  	"#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+  	self.characters.collect {|char| "#{char.name} - #{char.show.name}"}
+  end
+
 end
+
+
+
+
+
+
+# Write a method in the Actor class, #full_name, that returns the first and last
+# name of an actor. Write a method in the Actor class, #list_roles, that lists
+# all of the characters that actor has. 
